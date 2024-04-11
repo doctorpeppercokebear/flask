02fd0008034bf3f75af66dd7b67934b779c97d0a
momingyu <river98@hanmail.net>
2024-04-11 11:39:08 +0900

    계산 기능 추가

--- flask_first.py
+++ flask_first.py
@@ -2,15 +2,28 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])         #  메인 페이지
+
+@app.route('/', methods=['GET', 'POST'])
 def main():
-    if request.method == 'POST':            # 사용자가 post 방식으로 접속 했을 때
+    result = None  # result 변수 초기화
+    if request.method == 'POST':
         number1 = request.form.get('number1', type=float)
         number2 = request.form.get('number2', type=float)
-        print(number1, number1)
-    else:
-        return render_template('first_template.html')
+        operation = request.form.get('operation')
+        if operation == 'add':              # 덧셈
+            result = number1 + number2
+        elif operation == 'subtract':       # 뺄셈
+            result = number1 - number2
+        elif operation == 'multiplication':     # 곱셈
+            result = number1 * number2
+        elif operation == 'devide':     # 곱셈
+            if number2 !=0:
+                result = number1 * number2
+            else :
+                result = 'Invalid Error'
 
-if __name__ =='__main__':
+    return render_template('first_template.html', result=result)
+
+
+if __name__ == '__main__':
     app.run(debug=True)
-
